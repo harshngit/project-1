d5c5cd4e626311594f41ccda5ef744f9b1987138
Harsh Nikharge <158061768+harshngit@users.noreply.github.com>
2024-01-29 20:51:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	  <link href="http://fonts.cdnfonts.com/css/poppins" rel="stylesheet">
+</head>
+<body>
+	<!-- navbar section -->
+<header class="header">
+	<a href="#" class="logo"> Portfoili</a>
+	<nav class="navbar">
+		<a href="#home">Home</a>
+		<a href="#about">About me</a>
+		<a href="#skills">Skils</a>
+		<a href="#project">Project</a>
+		<a href="#contact">Contact me</a>
+	</nav>
+</header>
+<!--  -->
+
+<!-- home section start -->
+<section class="home" id="home">
+	<div class="home-content">
+		<h3>Hello, It's me</h3>
+		<h1>Harsh Nikharge</h1>
+		<h3>And I'm a <span class="text"></span></h3>
+		<p>I'm a web developer with extensive experience for over 3 year.
+			<br>Expertise is to create and website ,Frontend & Backend developer
+		</p>
+		<div class="home-sci">
+			<a href="#"><i class="fa-brands fa-whatsapp" style="color: #11fa00;"></i></a>
+			<a href="#"><i class="fa-brands fa-instagram" style="color: #11fa00;"></i></a>
+			<a href="#"><i class="fa-brands fa-linkedin" style="color: #11fa00;"></i></a>
+		</div>
+		<a href="#about" class="btn-box">More About me</a>
+	</div>
+</section>
+
+
+<!-- about section starts -->
+
+<section class="about" id="about">
+	<div class="about-img">
+		<img src="img/2.png">
+	</div>
+	<div class="about-text">
+		<h3>About 	<span>Me</span></h3>
+		<h4>Full stack developer</h4>
+		<p>I am Harsh Nikharge, studying in Atharva College Of Engineering
+		<br>doing BE in Electronics And Computer Science (ECS) (third year). 
+		<br> I had doneinternship in FDC Pharma company for month. I had help
+		<br>them in doing asset data entry website full stack developer by using 
+		<br>Html Css PHP and MYSQL. I am doing web developing since 3 year.
+		<br>My passion is to build website with creating various design as
+		<br>as per topic. I take pride to be up to date with new creative 
+		<br>design and leveraging my creative to produce visually appealing 
+		<br>By putting myself in the shoes of the end user, i stive to create 
+		<br>intutive seamless browsing experience. I believe that the website
+		<br>should not be the the medium to visite for browsing or research it
+		<br>it should be the smooth and enjoyable interaction for visitors
+		</p>
+		<a href="project" class="btn-box">Projects</a>
+	</div>
+</section>
+
+
+<!-- skilss section starts -->
+<h1 class="title">My <span>Skills</span></h1>
+ <section class="serviceoffers" id="skills">
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-6 col-md-12 col-10 offset-1 offset-lg-0">
+         <div class="names my-3">
+           <h1>HTML <i class="fa-brands fa-html5" style="color: #c95d2e;"></i></h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width:100%">100%</div>
+         </div>
+         <div class="names my-3">
+           <h1>JAVASCRIPT <i class="fa-brands fa-js" style="color: #b0bc1e;"></i></h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width:60%">60%%</div>
+         </div>
+         <div class="names my-3">
+           <h1>CSS<i class="fa-brands fa-css3-alt" style="color: #147bbc;"></i></h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" style="width:75%">75%</div>
+         </div>
+         <div class="names my-3">
+           <h1>PHP <i class="fa-brands fa-php" style="color: #0078F1;"></i></h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width:80%">80%</div>
+         </div>
+         <div class="names my-3">
+           <h1>MYSQL <i class="fa-solid fa-database"style="color: #11fa00;" ></i></h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-dark" style="width:80%">100%</div>
+         </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
+
+
+<!-- project section starts -->
+<section style="background-color:#19CB00;">
+	<div id="portfolio" id="project">
+		<div class="main-text" id="project">
+			<h2>Latest <span>Project</span></h2>
+
+			<div class="portfolio-content">
+				<div class="row">
+					<img src="img/p1.jpg">
+					<div class="layer">
+						<h5>Doublvison- E-commerce Website</h5>
+						<p>I create this e-commerece website with my four member team having skills about web development for my college project by adding my friends photo in it . I used php and mysql for recording data troung localhost </p>
+						<a href="#"><i class="fa-solid fa-arrow-up-right-from-square" style="color:#80FF33;"></i>	</a>
+					</div>
+				</div>
+				<div class="row">
+					<img src="img/p2.jpg">
+					<div class="layer">
+						<h5>YUM YUM - Food Website</h5>
+						<p>I create this food  website using youtube as refrence, it contain menu add to cart feature in it. I use javascript for animation and html and css for design </p>
+						<a href="#"><i class="fa-solid fa-arrow-up-right-from-square" style="color:#80FF33;"></i>	</a>
+					</div>
+				</div>
+				<div class="row">
+					<img src="img/p3.jpg">
+					<div class="layer">
+						<h5> Girly - Cosmetics Website</h5>
+						<p>I create this Cosmetics  website using youtube as refrence, it have various types of product of international brands . I use javascript for animation and html and css for design </p>
+						<a href="#"><i class="fa-solid fa-arrow-up-right-from-square" style="color:#80FF33;"></i>	</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- project section ends -->
+
+
+<!-- contact section start -->
+<h1 class="title"> Contact Me</h1>
+<section class="contactus" id="contactid">
+     <p class="text-capitalize pt-1">We're Here To Help And Answer Any Question You Might Have. We look Forward To Hearing From You 😀</p>
+   </div>
+
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-8 col-md-8 col-10 offset-lg-2 offset-md-2 offset-1"> 
+
+          <form  method="POST">
+           <div class="form-group">
+            <input type="text" class="form-control" name="username" placeholder="Enter Usermame" id="Username" required autocomplete="off">
+       </div>
+
+        <form action="/action_page.php">
+           <div class="form-group">
+            <input type="text" class="form-control" name="mobile" placeholder="Enter mobile number" id="Mobile Number" required autocomplete="off">
+       </div>
+        
+         <form action="/action_page.php">
+           <div class="form-group">
+            <input type="text" class="form-control" name="email" placeholder="Enter  Email address" id="Email" required autocomplete="off">
+       </div>
+       <div class="form-group">
+           <textarea class="form-control" rows="5" name="message" id="message" placeholder="Enter your messsage"  required autocomplete="off" ></textarea>
+       </div>
+       <a href="#"><input type="submit" name="submit" class="submit" placeholder="Submit"></a>
+      </form>
+       </div>
+     </div>
+   </div>
+</section>
+<div class="scrolltop float-right">
+      <i class="fa fa-arrow-up arrow" onclick="topFunction()" id="myBtn"></i>
+    </div>
+<!-- contact section end -->
+<!-- link -->
+<script src="main.js"></script> 
+<script type="text/javascript">
+	 //Get the button:
+    mybutton = document.getElementByclass("arrow")
+
+    //when the user scroll down 20px from the top the bottom of the document , show the buttom 
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body .scrollTop > 20 || document.documentElement.scrollTop > 20)  {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    // Whem=n the user clicks on the button, scroll to the top document
+    function topFunction() {
+      document.body .scrollTop = 0; // Foe Safari
+      document.documentElement.scrollTop = 0;// For Chrome , Firefox , IE and Opera 
+    }
+
+</script>
+</body>
+</html>
